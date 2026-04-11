fc1d8a1364754c5617fa7cd0366bacd1c60655ab
Selemani Lusukama Selestin <lusukamaselemani@gmail.com>
2026-04-11 20:20:27 +0200

    Umoja Learning Management System.
    but several defects have been identified. Your responsibility is to test the system, identify and document software defects, assist in debugging selected issues, and produce a final software testing report for management.

---
+++ ULMSWinFormsApp/Forms/FrmDashboard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ULMSWinFormsApp.Forms
+{
+    public partial class FrmDashboard : Form
+    {
+        public FrmDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            FrmLogin login = new FrmLogin();
+            login.Show();
+            this.Close();
+        }
+
+        private void btnStudentRegistration_Click(object sender, EventArgs e)
+        {
+            FrmStudentRegistration registrationForm = new FrmStudentRegistration();
+            registrationForm.ShowDialog();
+        }
+    }
+}
